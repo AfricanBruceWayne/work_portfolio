2342a244749c8da3932328cd5f09e13bb50b508f
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-12-11 18:09:01 +0200

    Updated server

--- app.js
+++ app.js
@@ -1,25 +1,91 @@
-const express = require('express'),
-      session = require('express-session'),
-      logger  = require('morgan'),
-      chalk   = require('chalk'),
-      lusca   = require('lusca'),
-      flash   = require('express-flash'),
-      path    = require('path'),
-      multer  = require('multer');
+'use strict';
+
+const express     = require('express'),
+      compression = require('compression'),
+      session     = require('express-session'),
+      logger      = require('morgan'),
+      chalk       = require('chalk'),
+      lusca       = require('lusca'),
+      flash       = require('express-flash'),
+      path        = require('path'),
+      passport    = require('passport'),
+      sass        = require('node-sass-middleware'),
+      multer      = require('multer');
 
 const upload = multer({
     dest: path.join(__dirname, 'uploads')
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+/**
+ * Controllers (route handlers).
+ */
+const homeController = require('./controllers/home');
 
-// set path for static assets
-app.use(express.static(path.join(__dirname, 'public')));
-
+/**
+ * Create Express server.
+ */
 const app = express();
 
-app.listen(() => {
-    console.log('Server has started');
+/**
+ * Express configuration.
+ */
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public')
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.use((req, res, next) => {
+    if (req.path === '/api/upload') {
+        res.locals.user = req.user;
+        next()
+    } else {
+        lusca.csrf()(req, res, next);
+    }
 });
+app.use((req, res, next) => {
+    // After successful login, redirect back to the intended page
+    if (!req.user
+      && req.path !== '/login'
+      && req.path !== '/signup'
+      && !req.path.match(/^\/auth/)
+      && !req.path.match(/\./)) {
+      req.session.returnTo = req.originalUrl;
+    } else if (req.user
+      && (req.path === '/account' || req.path.match(/^\/api/))) {
+      req.session.returnTo = req.originalUrl;
+    }
+    next();
+  });
+app.use(lusca.xframe('SAMEORIGIN'));
+app.use(lusca.xssProtection(true));
+app.disable('x-powered-by');
+app.use('/', express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/popper.js/dist/umd'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js'), { maxAge: 31557600000 }));
+app.use('/js/lib', express.static(path.join(__dirname, 'node_modules/jquery/dist'), { maxAge: 31557600000 }));
+app.use('/webfonts', express.static(path.join(__dirname, 'node_modules/@fortawesome/fontawesome-free/webfonts'), { maxAge: 31557600000 }));
+
+/**
+ * Primary app routes.
+ */
+app.get('/', homeController.index);
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), () => {
+    console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+});
+
+module.exports = app;
+
+
+
